0ae02d1cd55d4faf22dd83f9d55fd53d56a24943
Brittani <brittani.morganfield@gmail.com>
2021-07-01 00:20:17 -0500

    made changes to java to make work

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ console.log("You can watch Girl from Nowhere on Netflix");
 
 //declaring variable x, setting it to 20
 var x; //variable declaration
-x = 20;// variable declartion
+x = 20;// variable assignment
 
 console.log(x); //Prints out the value stored in x
 
@@ -12,9 +12,9 @@ console.log(myName);
 var userMessage = prompt("Have you seen Girl from Nowhere on Netflix?");
 console.log(userMessage);
 
-if (userMessage == No){
+if (userMessage = No){
 document.write("Thank you!");
 }
-also if (userMessage == Yes){
+else if(userMessage = Yes){
     document.write("Thank you!");
     }
