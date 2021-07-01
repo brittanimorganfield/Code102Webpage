7a5c1e73d5c559c92e4c78c2ec4f41bc69af978c
Brittani <brittani.morganfield@gmail.com>
2021-07-01 00:08:19 -0500

    added java

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log("You can watch Girl from Nowhere on Netflix");
+
+//declaring variable x, setting it to 20
+var x; //variable declaration
+x = 20;// variable declaration
+
+console.log(x); //Prints out the value stored in x
+
+var myName = "Brittani";
+console.log(myName);
+
+var userMessage = prompt("Have you seen Girl from Nowhere on Netflix?");
+console.log(userMessage);
+
+if (userMessage >= No){
+document.write("Thank you!")
+}
+also if (userMessage >= Yes){
+    document.write("Thank you!")
+    }
